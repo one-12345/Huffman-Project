f58c49a08ab9e66463fc54b7249c750c28c2ca6c
GamingWithAPengu <148139364+GamingWithAPengu@users.noreply.github.com>
2026-05-19 14:23:58 -0400

    hashMap building - msg2Bit

--- huffmanLookup.java
+++ huffmanLookup.java
@@ -1,5 +1,22 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
 public class huffmanLookup {
+
+
+
+
+    public HashMap<Character, String> buildTable(File tableFile) throws FileNotFoundException{
+        HashMap<Character, String> table = new HashMap();
+        Scanner scanner = new Scanner(tableFile);
+
+        while(scanner.hasNextLine()){
+           char nextChar = scanner.next().toCharArray()[0];
+           String byteString = scanner.next();
+           table.put(nextChar, byteString);
+        }
+        return table;
+    }
     public static String messageToBits(String s, HashMap table) {
         String out = "";
 
