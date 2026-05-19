a1c2cde04b6820aa4595ab7923ab665d531e4203
anne457 <147439338+anne457@users.noreply.github.com>
2026-05-19 14:18:32 -0400

    Implement file writing in messageToBits method
    Added file writing functionality to save output.

--- huffmanLookup.java
+++ huffmanLookup.java
@@ -1,13 +1,17 @@
-import java.util.*;
-public class huffmanLookup {
-    public static String messageToBits(String s, HashMap table) {
+ public static String messageToBits(String s, HashMap table) {
         String out = "";
-
         for(int i = 0; i< s.length(); i++) {
             int c = s.charAt(i)-65;
             String plus = table.get(c).toString();
             out +=plus;
         }
+        try(FileWriter fw = new FileWriter("messageToBits.txt")){
+            fw.write(out);
+        }
+        catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
         return out;
+
     }
-}
