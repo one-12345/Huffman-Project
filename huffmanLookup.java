572d24e9e8cd9bd65c69ba516a25387c0f62004f
anne457 <147439338+anne457@users.noreply.github.com>
2026-05-19 14:12:02 -0400

    Implement messageToBits using HashMap
    Refactor messageToBits method to use HashMap for character to bit conversion.

--- huffmanLookup.java
+++ huffmanLookup.java
@@ -1,3 +1,13 @@
+import java.util.*;
 public class huffmanLookup {
-    
+    public static String messageToBits(String s, HashMap table) {
+        String out = "";
+
+        for(int i = 0; i< s.length(); i++) {
+            int c = s.charAt(i)-65;
+            String plus = table.get(c).toString();
+            out +=plus;
+        }
+        return out;
+    }
 }
